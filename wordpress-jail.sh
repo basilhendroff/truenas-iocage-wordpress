16fe21709ffacad92451592a1b7e4ed094c00dd7
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2020-09-11 12:42:20 +0800

    php for jetpack
    php74-xmlrpc is required for jetpack to operate within the WordPress environment

--- wordpress-jail.sh
+++ wordpress-jail.sh
@@ -146,8 +146,8 @@ cat <<__EOF__ >/tmp/pkg.json
   "php74","php74-curl","php74-dom","php74-exif","php74-fileinfo","php74-json","php74-mbstring",
   "php74-mysqli","php74-pecl-libsodium","php74-openssl","php74-pecl-imagick","php74-xml","php74-zip",
   "php74-filter","php74-gd","php74-iconv","php74-pecl-mcrypt","php74-simplexml","php74-xmlreader","php74-zlib",
-  "php74-ftp","php74-pecl-ssh2","php74-sockets","mariadb103-server",
-  "unix2dos","ssmtp"
+  "php74-ftp","php74-pecl-ssh2","php74-sockets",
+  "mariadb103-server","unix2dos","ssmtp","php74-xmlrpc"
   ]
 }
 __EOF__
