28c79a96a21a25aa8ec6a8707b125c4d97110570
basilhendroff <basil.hendrof@gmail.com>
2020-10-18 21:20:54 +0800

    www#
    www#

---
+++ includes/wp-config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+infile="/usr/local/www/wordpress/wp-config.php"
+outfile="/usr/local/www/wordpress/wp-config.tmp"
+cat $infile | while IFS= read -r line; do
+  case $line in
+    "<?php")
+      printf '%s\n' "$line" >> $outfile
+      cat /mnt/includes/fragment-1.php | while IFS= read -r frag; do
+        printf '%s\n' "$frag" >> $outfile
+      done 
+      ;;
+    *DB_PASSWORD*)
+      printf '%s\n' "$line" >> $outfile
+      cat /mnt/includes/fragment-2.php | while IFS= read -r frag; do
+        printf '%s\n' "$frag" >> $outfile
+      done
+      ;;
+    "/* That's all, stop editing! Happy publishing. */")
+      cat /mnt/includes/fragment-3.php | while IFS= read -r frag; do
+        printf '%s\n' "$frag" >> $outfile
+      done
+      printf '%s\n' "$line" >> $outfile
+      ;;
+    *)
+      printf '%s\n' "$line" >> $outfile
+      ;;
+  esac
+done
+mv $outfile $infile
