9feaf26d4e48f0692e24d39905327a2b56a9e536
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2021-01-01 16:44:58 +0800

    Use Caddy 2.3.0
    Code changes to streamline use of the latest Caddy version.

--- wordpress-jail.sh
+++ wordpress-jail.sh
@@ -170,8 +170,10 @@ iocage fstab -a "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
 #####################################################################
 print_msg "Caddy download..."
 
-FILE="caddy_2.2.1_freebsd_amd64.tar.gz"
-if ! iocage exec "${JAIL_NAME}" fetch -o /tmp https://github.com/caddyserver/caddy/releases/latest/download/"${FILE}"
+CADDY_VERSION="2.3.0"
+
+FILE="caddy_${CADDY_VERSION}_freebsd_amd64.tar.gz"
+if ! iocage exec "${JAIL_NAME}" fetch -o /tmp https://github.com/caddyserver/caddy/releases/download/v"${CADDY_VERSION}"/"${FILE}"
 then
   print_err "Failed to download Caddy"
   exit 1
